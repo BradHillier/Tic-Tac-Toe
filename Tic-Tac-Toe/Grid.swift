8a6f62df1c2415bfe15600d6ad1491f9cf17e458
BradHillier <bradhillier@stumail.viu.ca>
2022-07-17 17:40:17 -0700

    added size property on Grid struct; method to get grid rows as a 2d array; CustomStringConvertible protocol

--- Tic-Tac-Toe/Grid.swift
+++ Tic-Tac-Toe/Grid.swift
@@ -7,9 +7,10 @@
 
 import Foundation
 
-struct Grid<Content> {
+struct Grid<Content>: CustomStringConvertible {
     
     private(set) var asLinearArrangement: Array<Cell>
+    private let size: Int
     
     struct Cell: Identifiable {
         /* only grid should need to see the id*/
@@ -17,7 +18,9 @@ struct Grid<Content> {
         private(set) var content: Content?
     }
     
+    // Todo: add ability to supply default content for cells
     init(size: Int) {
+        self.size = size
         asLinearArrangement = Array<Cell>()
         for number in 0..<size * size {
             asLinearArrangement.append(Cell(id: number))
@@ -30,5 +33,32 @@ struct Grid<Content> {
 
     func isEmpty() -> Bool {
         return asLinearArrangement.allSatisfy({ $0.content == nil })
+    }
+    
+    func rows() -> [Array<Grid<Content>.Cell>] {
+        var split = [[Cell]]()
+        var currentRow = [Cell]()
+        for cell in asLinearArrangement {
+            currentRow.append(cell)
+            if (cell.id + 1) % size == 0 {
+                split.append(currentRow)
+                currentRow.removeAll()
+            }
+        }
+        return split
+    }
+    
+    var description: String {
+        var des = ""
+        for row in rows() {
+            des += String(repeating: "-", count: size * 4 + 1)
+            des += "\n"
+            for cell in row {
+                des += "| \(cell.id) "
+            }
+            des += "|\n"
+        }
+        des += String(repeating: "-", count: size * 4 + 1)
+        return des
     }
 }
