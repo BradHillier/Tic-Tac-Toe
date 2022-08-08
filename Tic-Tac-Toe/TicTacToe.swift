c00a0e42ae9069b43cb39a8851584452b1aba72f
BradHillier <bradhillier@stumail.viu.ca>
2022-08-08 14:01:33 -0700

    added type alias for Grid<Player?>; added documentation

--- Tic-Tac-Toe/TicTacToe.swift
+++ Tic-Tac-Toe/TicTacToe.swift
@@ -9,12 +9,17 @@ import Foundation
 
 struct TicTacToe {
     
+    typealias Board = Grid<Player?>
+    
     private(set) var currentPlayer: Player
     private(set) var gridSize: Int
-    private(set) var board: Grid<Player?>
-    private var moves = Array<Grid<Player?>.Cell>()
-    private var undoneMoves = Array<Grid<Player?>.Cell>()
+    private(set) var board: Board
+    private(set) var moves = Array<Board.Cell>()
+    private var undoneMoves = Array<Board.Cell>()
     
+    /// representing the players of a TicTacToe game\
+    ///
+    /// - Note: the is a test note
     enum Player: CaseIterable {
         case X, O
         
@@ -28,18 +33,24 @@ struct TicTacToe {
         }
     }
     
-    static func emptyGameBoard(size: Int) -> Grid<Player?> {
+    /// Returns a `Grid` containing `nil` `Player` optionals of the provided size
+    static func emptyGameBoard(size: Int) -> Board {
         let emptyBoard = Grid<Player?>(size: size)
         return emptyBoard
     }
     
+    ///  Creates an instance with an empty board and `.X`
     init(gridSize: Int) {
         self.gridSize = gridSize
         board = TicTacToe.emptyGameBoard(size: gridSize)
         currentPlayer = .X
     }
     
-    mutating func choose(cell: Grid<Player?>.Cell) {
+    /// Change the content of the provided `cell` to the current `Player`;
+    /// only if the cell isn't already taken and the game isn't over
+    ///
+    ///  - Parameter cell: The cell the current player is attempting to choose
+    mutating func choose(cell: Board.Cell) {
         if cell.isEmpty() && winner() == nil {
             if let successfulMove = board.changeContent(of: cell, to: currentPlayer) {
                 moves.append(successfulMove)
@@ -50,6 +61,7 @@ struct TicTacToe {
         }
     }
     
+    /// undo the most recent move
     mutating func undo() {
         if !moves.isEmpty {
             let cell: Grid<Player?>.Cell = moves.removeLast()
@@ -60,6 +72,7 @@ struct TicTacToe {
         }
     }
     
+    /// redo the mostly recently undone move
     mutating func redo() {
         if !undoneMoves.isEmpty {
             let cell = undoneMoves.removeLast()
@@ -70,6 +83,11 @@ struct TicTacToe {
         }
     }
     
+    /// Returns the winning Player of a TicTacToe instance
+    ///
+    ///  Checks for a winner horizontally, vertically, and diagonally
+    /// - Complexity: O(n), where n is the size of the instance's board
+    /// - Returns: the winning `Player` if there is one; `nil` otherwise.
     func winner() -> Player? {
         for player in Player.allCases {
             for row in board.rows() {
@@ -91,15 +109,17 @@ struct TicTacToe {
         return nil
     }
     
+    /// Returns`true` if the game is over or there are no available moves;
+    /// otherwise return `false`
     func isTerminal() -> Bool {
         return winner() != nil || board.isFull()
     }
     
+    /// Restore the game to it's initial state
     mutating func reset() {
         board = TicTacToe.emptyGameBoard(size: gridSize)
         currentPlayer = .X
         moves.removeAll()
         undoneMoves.removeAll()
     }
-
 }
