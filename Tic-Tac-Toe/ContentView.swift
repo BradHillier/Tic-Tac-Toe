13634e364fe2a591e19d30272f71fb2893c810c4
BradHillier <bradhillier@stumail.viu.ca>
2022-08-11 16:12:18 -0700

    Player O is now blue and X is red; adjusted sizes; added ControlView

--- Tic-Tac-Toe/ContentView.swift
+++ Tic-Tac-Toe/ContentView.swift
@@ -27,43 +27,9 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .foregroundColor(.primary)
             Spacer()
-            Spacer()
             GridView(game: game)
-            Spacer()
-            Spacer()
-            HStack {
-                Spacer()
-                Button {
-                    game.reset()
-                } label: {
-                    VStack {
-                        Image(systemName: "restart")
-                        Text("restart")
-                    }
-                }
-                Spacer()
-                Button {
-                    game.undo()
-                } label: {
-                    VStack {
-                        Image(systemName: "arrow.uturn.backward")
-                        Text("undo")
-                    }
-                }
-                Spacer()
-                Button {
-                    game.redo()
-                } label: {
-                    VStack {
-                        Image(systemName: "arrow.uturn.forward")
-                        Text("redo")
-                    }
-                }
-                Spacer()
-            }
-            .padding()
-        }
-        .controlSize(.large)
+            Spacer(minLength: 70)
+            ControlView(game: game)
     }
 }
 
@@ -73,7 +39,7 @@ struct GridView: View {
     var body: some View {
         LazyVGrid (columns: Array(repeating: GridItem(), count: game.size), spacing: 8) {
             ForEach(game.board) { cell in
-                CellView(player: game.getValue(of: cell))
+                CellView(player: cell)
                     .aspectRatio(1/1, contentMode: .fit)
                     .onTapGesture {
                         game.choose(cell)
@@ -82,25 +48,88 @@ struct GridView: View {
         }
         .background(.primary)
         .padding()
-        
     }
 }
 
 struct CellView: View {
-    var player: Image?
+    var player: Grid<TicTacToe.Player?>.Cell
     
     var body: some View {
         ZStack {
             Rectangle()
                 .fill(.background)
-            if let playerImage = player {
-                playerImage
-                    .font(.largeTitle)
-                    .foregroundColor(.primary)
+            if let player = player.content  {
+                if player == .X {
+                    PlayerXView()
+                } else if player == .O {
+                    PlayerOView()
+                }
             }
         }
     }
 }
+
+struct ControlView: View {
+    var game: ImageTicTacToeGame
+    
+    var body: some View {
+        HStack {
+            Spacer()
+            Button {
+                game.reset()
+            } label: {
+                VStack {
+                    Image(systemName: "restart")
+                    Text("restart")
+                }
+            }
+            Spacer()
+            Button {
+                game.undo()
+            } label: {
+                VStack {
+                    Image(systemName: "arrow.uturn.backward")
+                    Text("undo")
+                }
+            }
+            Spacer()
+            Button {
+                game.redo()
+            } label: {
+                VStack {
+                    Image(systemName: "arrow.uturn.forward")
+                    Text("redo")
+                }
+            }
+            Spacer()
+        }
+        .padding()
+        .controlSize(.large)
+    }
+    }
+}
+
+struct PlayerXView: View {
+    var body: some View {
+        Image(systemName: "xmark")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .padding(20)
+            .foregroundColor(.red)
+    }
+}
+
+struct PlayerOView: View {
+    var body: some View {
+        Image(systemName: "circle")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .padding(15)
+            .foregroundColor(.blue)
+    }
+}
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
