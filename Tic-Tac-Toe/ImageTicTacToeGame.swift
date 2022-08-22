debbf5a710e3bfe365f09e90953cdcc59dae0853
BradHillier <bradhillier@stumail.viu.ca>
2022-08-22 14:02:22 -0700

    added method for getting the position where a line should appear on the screen

--- Tic-Tac-Toe/ImageTicTacToeGame.swift
+++ Tic-Tac-Toe/ImageTicTacToeGame.swift
@@ -13,7 +13,7 @@ class ImageTicTacToeGame: ObservableObject {
     let bot: TicTacToeBot
     
     init() {
-        game = TicTacToe(size: 3, winCondition: 3)
+        game = TicTacToe(size: 4, winCondition: 3)
         bot = TicTacToeBot(MaxDepth: 4)
     }
     
@@ -58,10 +58,38 @@ class ImageTicTacToeGame: ObservableObject {
         
     }
     
+    func getLinePositions(width: CGFloat, height: CGFloat) -> (start: CGPoint, end: CGPoint) {
+        var start: (row: Int, col: Int)
+        var end: (row: Int, col: Int)
+        
+        if let cells = game.winningCells() {
+            start = (row: cells.first!.column, col: cells.first!.row)
+            end = (row: cells.last!.column, col: cells.last!.row)
+        } else {
+            /// - ToDo: remove this
+            start = (row: 0, col: 0)
+            end = (row: 0, col: 0)
+        }
+        let deltaX = end.col - start.col
+        let deltaY = end.row - start.row
+        
+        let minDimension = Int(min(width, height))
+        let maxDimension = Int(max(width, height))
+        let cellSize = minDimension / game.size
+        let gridStartY = maxDimension - minDimension
+        
+        let startX = (cellSize * start.col) + (cellSize / 2)
+        let startY = gridStartY + cellSize / 2 + cellSize * start.row
+        
+        let endX = startX + deltaX * cellSize
+        let endY = startY + deltaY * cellSize
+        
+        return (start: CGPoint(x: startX, y: startY), end: CGPoint(x: endX, y: endY))
+    }
+    
     func aiMove() {
         if let move = bot.optimalMove(game) {
             game.choose(cell: move)
         }
     }
-    
 }
